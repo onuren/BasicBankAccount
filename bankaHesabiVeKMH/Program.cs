7005f7a87db1d83550edbe23040bd0adceffcc2b
onur engun <onurengun@outlook.com>
2021-01-20 03:33:55 +0300

    Add project files.

---
+++ bankaHesabiVeKMH/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankaHesabiVeKMH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hesap hsp = new hesap();
+
+            for (; ; )
+            {
+                hsp.baslangic();
+                string giris = Console.ReadLine();
+
+                if(giris== "Yükle" || giris=="1")
+                {
+                    Console.WriteLine("Yatırmak istediğiniz miktarı giriniz.");
+                    double i = Convert.ToDouble(Console.ReadLine());
+                    hsp.Yukle(i);
+                }
+                if (giris == "Ödeme" || giris == "2")
+                {
+                    Console.WriteLine("Ödemek istediğiniz miktarı giriniz.");
+                    double i = Convert.ToDouble(Console.ReadLine());
+                    hsp.odeme(i);
+                }
+                if (giris == "Bakiye" || giris == "3")
+                {
+                    hsp.bakiyeGoster();
+                }
+                if (giris == "Çıkış" || giris == "4")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ bankaHesabiVeKMH/hesap.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankaHesabiVeKMH
+{
+    class hesap
+    {
+        private double bankaHesabi;
+        private double KMHHesabi = 2000;
+        private double KMHHesabiLimiti = 2000;
+        private double KMHHesabiFaiz = 1.02;
+
+        public void bakiyeGoster()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Hesap Bakiyesi: " + bankaHesabi);
+            Console.WriteLine("Kredi Hesabı Bakiyesi: "+KMHHesabi);
+        }
+
+        public void Yukle(double miktar)
+        {
+            if(KMHHesabi==KMHHesabiLimiti)
+            {
+                bankaHesabi += miktar;
+            }
+            else
+            {
+                double KMHBorc = KMHHesabiLimiti - KMHHesabi;
+                KMHBorc *= KMHHesabiFaiz;
+                if(KMHBorc>=miktar)
+                {
+                    KMHHesabi += (miktar*=(1-(KMHHesabiFaiz-1)));
+                }
+                if(KMHBorc<miktar)
+                {
+                    bankaHesabi += (miktar - KMHBorc);
+                    KMHHesabi = KMHHesabiLimiti;
+                }
+            }
+            bakiyeGoster();
+        }
+        public void odeme(double miktar)
+        {
+            if (bankaHesabi>=miktar)
+            {
+                bankaHesabi -= miktar;
+            }
+            else if(bankaHesabi<=miktar && miktar<=(KMHHesabi+bankaHesabi))
+            {
+                KMHHesabi -= (miktar - bankaHesabi);
+                bankaHesabi = 0;
+            }
+            else
+            {
+                Console.WriteLine("Bakiye Yetersizdir.");
+            }
+            bakiyeGoster();
+        }
+        public void baslangic()
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Yapmak istediğiniz işlemi giriniz: ");
+            Console.WriteLine("     1* Yükle");
+            Console.WriteLine("     2* Ödeme");
+            Console.WriteLine("     3* Bakiye");
+            Console.WriteLine("     4* Çıkış");
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+}
